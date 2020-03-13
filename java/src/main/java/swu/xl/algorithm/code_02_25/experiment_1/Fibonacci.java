fc2b7ea8d1e1149c52b5d0aaccf23634a1653f66
xulei <2867584387@qq.com>
2020-03-13 16:35:30 +0800

    添加注释

--- java/src/main/java/swu/xl/algorithm/code_02_25/experiment_1/Fibonacci.java
+++ java/src/main/java/swu/xl/algorithm/code_02_25/experiment_1/Fibonacci.java
@@ -1,6 +1,14 @@
 package swu.xl.algorithm.code_02_25.experiment_1;
 
+/**
+ * 求解斐波那契数
+ */
 public class Fibonacci {
+    /**
+     * 递归法
+     * @param n
+     * @return
+     */
     public static long getByRecursion(long n){
         if(n == 1 || n == 2){
             return 1;
@@ -9,6 +17,11 @@ public class Fibonacci {
         return getByRecursion(n-1) + getByRecursion(n-2);
     }
 
+    /**
+     * 迭代法
+     * @param n
+     * @return
+     */
     public static long getByIteration(long n){
         long a = 1;
         long b = 1;
--- java/src/main/java/swu/xl/algorithm/code_02_25/experiment_2/GCD.java
+++ java/src/main/java/swu/xl/algorithm/code_02_25/experiment_2/GCD.java
@@ -1,8 +1,18 @@
 package swu.xl.algorithm.code_02_25.experiment_2;
 
+/**
+ * 求解最大公因数
+ */
 public class GCD {
+    /**
+     * 暴力枚举法
+     * @param a
+     * @param b
+     * @return
+     */
     public static int getByBruteForce(int a, int b){
         int result = 0;
+
         for (int i = Math.min(a, b); i > 0; i--) {
             if (a % i == 0 && b % i == 0){
                 result = i;
@@ -13,6 +23,12 @@ public class GCD {
         return result;
     }
 
+    /**
+     * 欧几里得法
+     * @param a
+     * @param b
+     * @return
+     */
     public static int getByEuclid(int a, int b){
         while(a != 0 && b != 0){
             if(a > b){
@@ -25,8 +41,12 @@ public class GCD {
         return a+b;
     }
 
-//    public static void main(String[] args) {
-//        System.out.println(getByEuclid(2,12));
-//        System.out.println(getByBruteForce(2,12));
-//    }
+    /**
+     * 测试函数
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println(getByEuclid(2,12));
+        System.out.println(getByBruteForce(2,12));
+    }
 }
