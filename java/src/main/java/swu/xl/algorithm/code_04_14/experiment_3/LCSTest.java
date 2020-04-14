d431b5910a0da1d892a60f93dadf0fe3050b3d1f
xulei <2867584387@qq.com>
2020-04-15 00:00:33 +0800

    第八周算法实验-commit-test

--- java/src/main/java/swu/xl/algorithm/code_04_14/experiment_3/LCSTest.java
+++ java/src/main/java/swu/xl/algorithm/code_04_14/experiment_3/LCSTest.java
@@ -3,7 +3,7 @@ package swu.xl.algorithm.code_04_14.experiment_3;
 public class LCSTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//TODO Auto-generated method stub
 		String str1 = "ABCBDAB";
 		String str2 = "BDCABA";
 		String str = LCS.getLCS(str1,str2);
